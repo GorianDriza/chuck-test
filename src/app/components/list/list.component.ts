cfc8af9ceba9eb0ae2bd4f34d9fd6077701cf8a4
Gorian Driza <goriandriza11@gmail.com>
2020-03-18 17:52:32 +0100

    add fact to  favorite with button and drag and drop. Change state when you move facts.

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -4,6 +4,8 @@ import {ListFactService} from '../../shared/services/list-fact.service';
 import {NgRedux} from '@angular-redux/store';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -12,7 +14,7 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 export class ListComponent implements OnInit {
   fetchedFacts = [];
   favoriteFacts = [];
-  test: any;
+  param: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -20,16 +22,44 @@ export class ListComponent implements OnInit {
     private ngRedux: NgRedux<any>
   ) {
     this.route.params.subscribe((param) => {
-      if (param && param.name) { this.listFactsService.getListOfFacts(param.name); }
+      this.param = param;
+      if (param && param.name) {
+        this.listFactsService.getListOfFacts(param.name);
+      }
     });
   }
 
   ngOnInit() {
     this.ngRedux.subscribe(() => {
       const factList = this.ngRedux.getState();
-      this.fetchedFacts = factList.facts;
+      this.fetchedFacts = this.filterFacts(factList);
     });
   }
+
+  filterFacts(factList) {
+    let facts = [];
+    if (this.favoriteFacts && this.favoriteFacts.length > 0) {
+      facts = _.differenceBy(factList.facts, this.favoriteFacts, 'id');
+    } else {
+      facts = factList.facts;
+    }
+    return facts;
+  }
+
+  addToFavorite(fact: any) {
+    this.favoriteFacts.push(fact);
+    if (this.param && this.param.name) {
+      this.listFactsService.getListOfFacts(this.param.name);
+    }
+  }
+
+  removeFromArray(fact: any) {
+    this.favoriteFacts = this.favoriteFacts.filter((favorite) => favorite.id !== fact.id);
+    if (this.param && this.param.name) {
+      this.listFactsService.getListOfFacts(this.param.name);
+    }
+  }
+
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
